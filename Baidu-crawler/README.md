9374a104fa1afe63223e7b80e105e5eac2d5eae7
TT375S <tomataka375@gmail.com>
2017-10-18 20:16:54 +0900

    Fix Baidu crawler

--- Baidu-crawler/README.md
+++ Baidu-crawler/README.md
@@ -1,13 +1,17 @@
 # short URL collector from Baidu
-*N/A*
+This program collects short url with Baidu search.
 
 ## Installation
-*N/A* 
+It needs Python3, GoogleChrome, selene and BeautifulSoup.
+
 ## Usage
-*N/A*
+`python3 shortURLCrawler-withBaidu.py < ../service-domain.txt > res.txt`  
+Then, remove duplication.  
+`sort res.txt | uniq`
 
-## In japanese... 
-いちおう動くけどBaiduの検索結果が良くないので正しく動いてるかよくわからない  
-
+## In japanese...
+Baiduはクローリングするドメインを制限しているのか、goo.glなどクローリングしていないドメインがいくつかある。  
+それ以外ならちゃんと結果を返すしロボットががんがんアクセスしても注意されない。  
+なかなか便利。
 
 
--- Baidu-crawler/shortURLCrawler-withBaidu.py
+++ Baidu-crawler/shortURLCrawler-withBaidu.py
@@ -20,33 +20,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from bs4 import BeautifulSoup
 
-#https://qiita.com/zarchis/items/3258562ebc9570fa05a3
-def conv_encoding(data):
-    lookup = ('utf_8', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213',
-              'shift_jis', 'shift_jis_2004','shift_jisx0213',
-              'iso2022jp', 'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_3',
-              'iso2022_jp_ext','latin_1', 'ascii')
-    encode = None
-    for encoding in lookup:
-        try:
-            data = data.decode(encoding)
-            encode = encoding
-            break
-        except:
-            pass
-        if isinstance(data, unicode):
-            return data,encode
-    else:
-        raise LookupError
-
-maxPage = 1
-maxItems = 10
+maxPage = 10
 
 # run chrome headless
 options = Options()
-options.add_argument('--headless')
+#options.add_argument('--headless')
 # install chromedriver if not found and start chrome
-driver = SeleneDriver.wrap(webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options))
+rawDriver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
+driver = SeleneDriver.wrap(rawDriver)
+
 
 domainNames = []
 #Input target short url service's domain names.
@@ -56,39 +38,54 @@ try:
 except EOFError:
     pass
 
-for domainName in domainNames:        
+for domainName in domainNames:
+    urlCount = 0        
+    print(domainName, file = sys.stderr)
+    
+    baiduURL = "http://www.baidu.com"
+    driver.get(baiduURL)
+    time.sleep(2)
+    #print(rawDriver.current_url)
+
+    #Enter keyword and search.
+    input_element = driver.find_element_by_name('wd')
+    input_element.send_keys("site:("+domainName + ")")
+    input_element.send_keys(Keys.RETURN)
+   
+    time.sleep(2) 
+    searchPageUrl = rawDriver.current_url
+
     for page in range(1,maxPage+1):
-        #time.sleep(1)
+        currentSearchPageUrl = searchPageUrl + "&pn=" + str(10*(page-1))
 
-        #TODO:(What is the "rsv_pq" query parameter? Is it somthing like session-id?)
-        searchURL = "http://www.baidu.com/s?wd=tinyurl.com&pn=" +str(page)+"&oq="  +domainName+"/" +"&ie=utf-8&usm=1&rsv_idx=1&rsv_pq=d7aa019b0002a564&rsv_t=b33dQMjJmCQStNYNt4kMjLoJ58nJKp3tISWBSXPeMIFMT8EeFwLkoEVNNjc"
+        #print(searchURL, file = sys.stderr)
+        driver.get(currentSearchPageUrl)
+        time.sleep(3.0)
 
-        driver.get(searchURL)
-        time.sleep(1.0)
-                
+        #driver.save_screenshot('result_'+ domainName +"_"  + str(page)  +'.png')        
         html = driver.page_source
         #print(html, file=sys.stderr)
-        
+
         #Converting soup object.
         soup = BeautifulSoup(html, "html.parser")
         searchResults = soup.find_all(class_ = "result c-container ")
-       
+        
+        urlCountInThisPage = 0
         #Checking each search result items.
         for searchResult in searchResults:
-            linkURL = (searchResult.find("a")).get("href")
-            print(linkURL)
-            try:
-                #Finding urls in the search result web site.
-                driver.get(linkURL)
-                time.sleep(1)
-                
-                #body,encoding = conv_encoding(response.read() )
-                body = driver.page_source
-                
-                urls = re.findall('(?:https?:\/\/|)'+ domainName  +'\/[0-9a-zA-Z]+' , body )
-                for url in urls:
-                    print(url.replace("https://", "").replace("http://", "") )
             
-            except urllib.error.HTTPError:
-                print("404", file=sys.stderr)
+            #Finding urls in the search result web site.
+            siteDescriptor = (searchResult.find(class_ = "c-showurl") ).string
+            linkURL = re.findall('(?:https?:\/\/|)'+ domainName  +'\/[0-9a-zA-Z]+' , siteDescriptor )
+            
+            if len(linkURL) <1:
+                continue
+            urlCountInThisPage += 1
+            urlCount += 1
+            print(linkURL[0].replace("https://", "").replace("http://", "") )
+        
+        #Result is empty.
+        if urlCountInThisPage == 0:
+            break
+    print(str(urlCount), file = sys.stderr)
 driver.quit()
