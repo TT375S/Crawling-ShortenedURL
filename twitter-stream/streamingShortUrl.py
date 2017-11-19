b59d40507fdb868aa3b46f885899b316a363b197
TT375S <tomataka375@gmail.com>
2017-11-19 20:59:38 +0900

    Fix twitter-stream. It is checked operation on Ubuntu

--- twitter-stream/streamingShortUrl.py
+++ twitter-stream/streamingShortUrl.py
@@ -4,6 +4,7 @@ import json
 import re
 import sys
 import datetime
+import traceback
 
 logFileData = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
 
@@ -27,9 +28,10 @@ auth = OAuth1(api_key, api_secret, access_token, access_secret)
 #r = requests.post(url, auth=auth, stream=True, data={"follow":"nasa9084","track":"emacs"})
 r = requests.post(url, auth=auth, stream=True, data={"track": keywords})
 
+
 for line in r.iter_lines():
     try:
-        jsonData = json.loads(line)
+        jsonData = json.loads(line.decode())
         print(jsonData["text"])
 
         #collect URL matched some patterns such as "http://aaa/aaaa" or "https://aaaaa/aa"
@@ -43,9 +45,8 @@ for line in r.iter_lines():
                 f.write(url + "\n")
                 f.close()
 
-        #print(jsonData)
     except:
-        print("ERR")
+        print(traceback.format_exc())
 
 
 
