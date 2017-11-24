ef75bc801e80055214e172a7af38a7cfb1edbb2d
TT375S <tomataka375@gmail.com>
2017-11-24 21:33:31 +0900

    Fix brutefore. Add all domains to domainSpecific.py

--- bruteForce/domainSpecific.py
+++ bruteForce/domainSpecific.py
@@ -16,4 +16,38 @@ class owly(baseDomainAgent):
     
     def isValid(self, shortURL, destURL, html):
         return not shortURL == destURL
-        
+
+class bitly(baseDomainAgent):
+    def __init__(self):
+        self.url = "http://bit.ly/"
+    
+    def isValid(self, shortURL, destURL, html):
+        return  not "does not exist" in html
+
+class tinyurl(baseDomainAgent):
+    def __init__(self):
+        self.url = "http://tinyurl.com/"
+    
+    def isValid(self, shortURL, destURL, html):
+        return not "Error: Unable to find URL to redirect to." in html
+
+class isgd(baseDomainAgent):
+    def __init__(self):
+        self.url = "http://is.gd/"
+    
+    def isValid(self, shortURL, destURL, html):
+        return not "The link you followed may be invalid" in html
+
+class tco(baseDomainAgent):
+    def __init__(self):
+        self.url = "http://t.co/"
+    
+    def isValid(self, shortURL, destURL, html):
+        return not ("このページは存在しません" in html or shortURL == destURL)
+
+class prtnu(baseDomainAgent):
+    def __init__(self):
+        self.url = "http://prt.nu/"
+    
+    def isValid(self, shortURL, destURL, html):
+        return not "このURLは有効ではありません" in html
