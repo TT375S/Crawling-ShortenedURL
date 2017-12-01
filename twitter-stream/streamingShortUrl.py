65ca494342ba69feb2b2a4ce68c4c4ca0c5d9a3e
TT375S <tomataka375@gmail.com>
2017-12-01 14:24:14 +0900

    Add wait if results is empty in twitter-stream.

--- twitter-stream/streamingShortUrl.py
+++ twitter-stream/streamingShortUrl.py
@@ -24,44 +24,45 @@ api_secret = ""
 access_token = ""
 access_secret = ""
 
-
 url = "https://stream.twitter.com/1.1/statuses/filter.json"
 
-auth = OAuth1(api_key, api_secret, access_token, access_secret)
 
-#r = requests.post(url, auth=auth, stream=True, data={"follow":"nasa9084","track":"emacs"})
-r = requests.post(url, auth=auth, stream=True, data={"track": keywords})
+while True:
+    print("connecting...")
+    auth = OAuth1(api_key, api_secret, access_token, access_secret)
+    
+    #r = requests.post(url, auth=auth, stream=True, data={"follow":"nasa9084","track":"emacs"})
+    r = requests.post(url, auth=auth, stream=True, data={"track": keywords})
 
 
-for line in r.iter_lines():
-    try:
-        jsonData = json.loads(line.decode())
-        print(jsonData["text"])
-    except:
-        print(traceback.format_exc())
-        time.sleep(5)
-        continue
-    else:
-        #collect URL matched some patterns such as "http://aaa/aaaa" or "https://aaaaa/aa"
-        for url in re.findall(
-                'https?://[\w:%#\$&\?\(\)~\.=\+\-]+/[\w/:%#\$&\?\(\)~\.=\+\-]+',
-                jsonData["text"]):
-            print(url)
-            #write log
-            if len(sys.argv) >= 2:
-                f = open(sys.argv[1] + "-" + logFileData + ".txt", "a")
-                f.write(url + "\n")
-                f.close()
-
-# except KeyError:
-#     print("KeyError:  maybe connection broken")
-#     time.sleep(20)
-#     r = requests.post(url, auth=auth, stream=True, data={"track": keywords})
-#     continue
-# except http.client.IncompleteRead:
-#     #except urllib3.exceptions.ProtocolError:
-#     print("ProtocolError: maybe connection broken")
-#     time.sleep(20)
-#     r = requests.post(url, auth=auth, stream=True, data={"track": keywords})
-#     continue
-
+    for line in r.iter_lines():
+        try:
+            jsonData = json.loads(line.decode("utf-8"))
+            print(jsonData["text"])
+        except KeyError:
+            print(traceback.format_exc())
+            #KeyError occure if result is empty. So, wait to return results.
+            time.sleep(10)
+            continue
+        except:
+            print(traceback.format_exc())
+            print("disconnect in 5 seconds")
+            
+            #may don't well work
+            #disconnect
+            #r = requests.post(url=url, auth=auth, stream=True, data={"track": keywords}, headers={'Connection':'close'})
+            r = requests.post(url=url, data={"track": keywords}, headers={'Connection':'close'}) 
+            time.sleep(10)
+            #r = requests.post(url=url, data=body, headers={'Connection':'close'})
+            #continue
+            break
+        else:
+            #collect URL matched some patterns such as "http://aaa/aaaa" or "https://aaaaa/aa"
+            for url in re.findall(
+                    'https?://[\w:%#\$&\?\(\)~\.=\+\-]+/[\w/:%#\$&\?\(\)~\.=\+\-]+',
+                    jsonData.get("text")):
+                print(url)
+                #write log
+                if len(sys.argv) >= 2:
+                    f = open(sys.argv[1] + "-" + logFileData + ".txt", "a")
+                    f.write(url + "\n")
