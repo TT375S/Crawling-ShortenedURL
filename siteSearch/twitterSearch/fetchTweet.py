490b78bdba63dec9b728b0c7679ec56e2afa84d6
TT375S <tomataka375@gmail.com>
2017-12-04 21:27:29 +0900

    Enable twitterSearch to collect shortURL and destURL

--- siteSearch/twitterSearch/fetchTweet.py
+++ siteSearch/twitterSearch/fetchTweet.py
@@ -1,37 +1,39 @@
+import datetime
+def writeLineOpenAndClose(fileName, mode, content):
+    f = open(fileName, mode)
+    f.write(content+"\n")
+    f.close()
+
+logFileData = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
+
 #most of this script came from https://www.karambelkar.info/2015/01/how-to-use-twitters-search-rest-api-most-effectively./
 import tweepy
 from tweepy import OAuthHandler
 
+# Replace the API_KEY and API_SECRET with your application's key and secret.
 ckey  = ""
 csecret = ""
 atoken = ""
 asecret = ""
 
-# Replace the API_KEY and API_SECRET with your application's key and secret.
-#auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 # OAuth
-#api = tweepy.API(auth)
-
-api = tweepy.API(auth, wait_on_rate_limit=True,
-				   wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 if (not api):
     print ("Can't Authenticate")
     sys.exit(-1)
-
-# Continue with rest of code
 
 import sys
 import jsonpickle
 import os
 
 searchQuery = 'http'  # this is what we're searching for
-maxTweets = 10000000 # Some arbitrary large number
-tweetsPerQry = 100  # this is the max the API permits
-fName = 'tweets.txt' # We'll store the tweets in a text file.
+maxTweets = 10000000  # Some arbitrary large number
+tweetsPerQry = 100    # this is the max the API permits
+fName = "tweets"+logFileData+".txt"  # We'll store the tweets in a text file.
 
 
 # If results from a specific ID onwards are reqd, set since_id to that ID.
@@ -64,7 +66,20 @@ with open(fName, 'w') as f:
             if not new_tweets:
                 print("No more tweets found")
                 break
+            #process tweets
             for tweet in new_tweets:
+                #print(tweet.user.name)
+                #print(tweet.user.screen_name)
+                #print(tweet.text)
+                #print(tweet.created_at+ datetime.timedelta(hours=9))
+                urlDict = tweet.entities["urls"]
+                for urlEntry in urlDict:
+                    sURL = urlEntry["url"]
+                    dURL = urlEntry["expanded_url"]
+                    print(sURL)
+                    print(dURL)
+                    writeLineOpenAndClose("sURL-"+logFileData+".txt", "a", sURL)
+                    writeLineOpenAndClose("dURL-"+logFileData+".txt", "a", dURL)
                 f.write(jsonpickle.encode(tweet._json, unpicklable=False) +
                         '\n')
             tweetCount += len(new_tweets)
