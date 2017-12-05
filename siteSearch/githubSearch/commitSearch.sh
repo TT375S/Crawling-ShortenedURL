b2b1af66760c4a28b8aab890bebdf6763dae6159
TT375S <tomataka375@gmail.com>
2017-12-05 12:32:45 +0900

    Change fetch algo in commitSearch.sh

--- siteSearch/githubSearch/commitSearch.sh
+++ siteSearch/githubSearch/commitSearch.sh
@@ -3,9 +3,11 @@
 
 for pageNum in `seq 3`
 do
-    curl -o temp.txt -H "Authentication: token TOKEN" \
-    -H "Accept: application/vnd.github.cloak-preview" \
-    https://api.github.com/search/commits?q=bit.ly&page=${pageNum:=1} ;
-    sleep 2s;
-    python3 commitDigest.py < temp.txt;
+    #curl -o temp.txt -H "Authentication: token TOKEN" \
+    #-H "Accept: application/vnd.github.cloak-preview" \
+    #https://api.github.com/search/commits?q=bit.ly&page=$( echo "${pageNum}");
+    
+    curl -H "Authentication: token TOKEN"     -H "Accept: application/vnd.github.cloak-preview"   "https://api.github.com/search/commits?q=bit.ly&per_page=100&page=$(echo "${pageNum}" )"  |    python3 commitDigest.py; 
+
+    echo pageNum = $( echo "${pageNum}")
 done
