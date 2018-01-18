9c27d916761acf3422c0426ce941dbf674321d63
TT375S <tomataka375@gmail.com>
2018-01-19 00:11:15 +0900

    Add cleansing Filter for BFA dURL file

---
+++ bruteForce/cleansingDUrlFile.py
@@ -0,0 +1,12 @@
+#なぜかdURLファイルにはb'とか\nとかが入るのでこれで削除する
+
+try:
+    while(1):
+        unCleansedUrl = input()
+
+        cleansedUrl = unCleansedUrl[2:len(unCleansedUrl)-1 -4]
+        print(cleansedUrl)
+except EOFError:
+    pass
+
+excep
