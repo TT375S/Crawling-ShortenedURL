095340dcdeed239aa4ee00427c48ace1f41668a8
TT375S <tomataka375@gmail.com>
2018-01-19 23:52:12 +0900

    Made aggregateJson from GoogleSafeBrowsing to output CSV

--- linkSafetyChecker/aggregateResultsOfGoogleSafeBrawsing.py
+++ linkSafetyChecker/aggregateResultsOfGoogleSafeBrawsing.py
@@ -7,6 +7,19 @@ urlDic = {}
 threatTypeDic = {}
 platformTypeDic = {}
 
+defaultThreatType = ["MALWARE", "SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE", "POTENTIALLY_HARMFUL_APPLICATION", "THREAT_TYPE_UNSPECIFIED"]
+defaultPlatformType = ["WINDOWS", "OSX","LINUX",  "ANDROID",  "IOS", "CHROME", "PLATFORM_TYPE_UNSPECIFIED"]
+    
+for threat in defaultThreatType:
+    threatTypeDic.setdefault( threat, {})
+    for platform in defaultPlatformType:
+        threatTypeDic[threat].setdefault(platform, 0)
+
+for platform in defaultPlatformType:
+    platformTypeDic.setdefault( platform, {})
+    for threat in defaultThreatType:
+        platformTypeDic[platform].setdefault(threat, 0)
+
 for result in data["matches"]:
     url = result["threat"]["url"]
     threatType = result["threatType"]
@@ -15,14 +28,24 @@ for result in data["matches"]:
     urlDic.setdefault(url, []) 
     urlDic[url].append( (threatType, platformType) )
     
-    threatTypeDic.setdefault( threatType, {})
-    threatTypeDic[threatType].setdefault(platformType, 0)
     threatTypeDic[threatType][platformType] += 1
 
-    
-    platformTypeDic.setdefault( platformType, {})
-    platformTypeDic[platformType].setdefault(threatType, 0)
     platformTypeDic[platformType][threatType] += 1
 #print(urlDic)
-print(threatTypeDic)
-print(platformTypeDic)
+
+print("   ,", end='')
+for platform in defaultPlatformType:
+    print(platform +", ", end='')
+print("")
+
+for k, v in threatTypeDic.items():
+    print(k +", ", end = '')
+    for j, n in v.items():
+        print(str(n) +", ", end = '')
+    print("")
+
+#for k, v in platformTypeDic.items():
+#    print(k + ", " + str(v))
+
+#print(threatTypeDic)
+#print(platformTypeDic)
